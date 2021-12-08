3da43492fafe42e303e1cbfb373fc0e4ba0af4cb
kkinge79 <kingerykagan@gmail.com>
2021-12-08 15:11:16 -0600

    add navlink with burgershop

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
 // Components & Assets
 import Logo from '../../assets/react-logo.png'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   console.log(Logo)
   return (
     <nav className="navigation-bar">
-
+      <NavLink to="/"><img src="logo192.png"/></NavLink>
+      <NavLink to="/burgers">BurgerShop</NavLink> | {" "}
     </nav>
   )
 }
